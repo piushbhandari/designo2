d9e3d157c4de0e05b07eed2954539ef3e0878753
piush bhandari <piushthegreat@piushs-MBP.attlocal.net>
2022-10-05 23:17:23 -0500

    first commit

---
+++ src/components/banner/Banner.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./Banner.module.css";
+const Banner = () => {
+  return <div>Banner</div>;
+};
+
+export default Banner;
---
+++ src/components/semifooter/SemiFooter.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./SemiFooter.module.css";
+
+const SemiFooter = () => {
+  return <div>SemiFooter</div>;
+};
+
+export default SemiFooter;
