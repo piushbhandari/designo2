4f1c81e48102c078537c30c35acdf5bb696fc065
piush bhandari <piushthegreat@piushs-MBP.attlocal.net>
2022-10-22 00:50:06 -0500

    nav almost done except mobile

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,62 @@
-import React from "react";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/no-redundant-roles */
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
+import logo from "./../../assets/shared/desktop/logo-dark.png";
+import hamburger from "./../../assets/shared/mobile/icon-hamburger.svg";
+import close from "./../../assets/shared/mobile/icon-close.svg";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  function handleMenuBtn(e) {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+  }
+  return (
+    <header className={styles.header}>
+      <div className={`${styles["header__container"]}`}>
+        <div className={styles.logo}>
+          <a href="/">
+            <img className="fit-image" src={logo} alt="designo company" />
+          </a>
+        </div>
+        <nav className={styles.nav}>
+          <div
+            className={`${styles["nav-links-container"]} ${
+              isOpen ? styles["open-nav"] : ""
+            }`}
+          >
+            <ul className={`${styles["nav-links"]}  `} role="list">
+              <li className={styles["nav-links__item"]}>
+                <a href="#" className={styles["nav-links__link"]}>
+                  Our Company
+                </a>
+              </li>
+              <li className={styles["nav-links__item"]}>
+                <a href="#" className={styles["nav-links__link"]}>
+                  locations
+                </a>
+              </li>
+              <li className={styles["nav-links__item"]}>
+                <a href="#" className={styles["nav-links__link"]}>
+                  contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          <button
+            className={styles["nav-links__toggle-menu"]}
+            onClick={handleMenuBtn}
+          >
+            <img
+              src={isOpen ? close : hamburger}
+              alt={isOpen ? "close menu" : "open menu"}
+            />
+          </button>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
