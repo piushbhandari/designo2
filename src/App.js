cbab746666d1584fab2c3bd134129225294da893
piush bhandari <piushthegreat@piushs-MBP.attlocal.net>
2022-10-23 14:48:09 -0500

    finished semi footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
+import Semifooter from "./components/semifooter/SemiFooter";
 import Footer from "./components/footer/Footer";
 const App = () => {
   return (
     <>
       <Navbar />
+      <Semifooter />
       <Footer />
     </>
   );
--- src/components/semifooter/SemiFooter.js
+++ src/components/semifooter/SemiFooter.js
@@ -1,8 +1,39 @@
 import React from "react";
 import styles from "./SemiFooter.module.css";
+import pattern2 from "./../../assets/shared/desktop/bg-pattern-two-circles.svg";
+import pattern3 from "./../../assets/shared/desktop/bg-pattern-three-circles.svg";
 
 const SemiFooter = () => {
-  return <div>SemiFooter</div>;
+  return (
+    <section className="section-semifooter">
+      <div className={`${styles["section-semifooter__container"]} container`}>
+        <div className={styles["section-semifooter__wrapper"]}>
+          <div
+            className={`${styles["section-semifooter__pattern"]} ${styles["section-semifooter__pattern--top"]}`}
+          >
+            <img src={pattern2} alt="circles" />
+          </div>
+          <div className="col1">
+            <h2 className={styles["section-semifooter__title"]}>
+              Let’s talk about your project
+            </h2>
+            <p className={styles["section-semifooter__text"]}>
+              Ready to take it to the next level? Contact us today and find out
+              how our expertise can help your business grow.
+            </p>
+          </div>
+          <button className={styles["section-semifooter__cta"]}>
+            Get in touch
+          </button>
+          <div
+            className={`${styles["section-semifooter__pattern"]} ${styles["section-semifooter__pattern--bottom"]}`}
+          >
+            <img src={pattern2} alt="circles" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SemiFooter;
